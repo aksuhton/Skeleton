b7bc6f75e5b004870cdb90ecb5e5ed345c719fbc
aksuhton <aksuhton@gmail.com>
2024-12-30 12:44:39 -0700

    begin workflow tutorial

---
+++ scripts/prep_sims.jl
@@ -0,0 +1,61 @@
+using DrWatson
+@quickactivate "Skeleton"
+using DataFrames
+
+# Here you may include files from the source directory
+include(srcdir("dummy_src_file.jl"))
+
+println(
+"""
+Currently active project is: $(projectname())
+
+Path of active project: $(projectdir())
+"""
+)
+##
+a, b = 2, 3
+v = rand(5)
+method = "linear"
+r, y = fakesim(a, b, v, method)
+##
+params = @strdict a b v method
+##
+allparams = Dict(
+    "a" => [1, 2],         # it is inside vector. It is expanded.
+    "b" => [3, 4],         # same
+    "v" => [rand(1:2, 2)], # single element inside vector; no expansion
+    "method" => "linear",  # not in vector = not expanded, even if naturally iterable
+)
+
+dicts = dict_list(allparams)
+##
+for (i, d) in enumerate(dicts)
+    f = makesim(d)
+    wsave(datadir("simulations", "sim_$(i).jld2"), f)
+end
+##
+savename(params)
+##
+savename(dicts[1], "jld2")
+##
+for (i, d) in enumerate(dicts)
+    f = makesim(d)
+    wsave(datadir("simulations", savename(d, "jld2")), f)
+end
+
+readdir(datadir("simulations"))
+##
+for (i, d) in enumerate(dicts)
+    f = makesim(d)
+    @tagsave(datadir("simulations", savename(d, "jld2")), f)
+end
+##
+firstsim = readdir(datadir("simulations"))[1]
+
+wload(datadir("simulations", firstsim))
+##
+df = collect_results(datadir("simulations"))
+##
+analysis = 42
+
+safesave(datadir("ana", "linear.jld2"), @strdict analysis)
