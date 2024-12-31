0ddc239aec8e3a8cf673e6e6974d845cb6d42c38
aksuhton <aksuhton@gmail.com>
2024-12-30 23:51:37 -0700

    Try: weakdeps and pkg

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 using DrWatson
 @quickactivate "Skeleton"
+Pkg.add("Documenter")
 using Documenter
 
 # Here you may include files from the source directory
