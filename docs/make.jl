cacaea1febe373158593451d57326716ba16e615
aksuhton <7018341+aksuhton@users.noreply.github.com>
2025-01-06 00:41:57 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -10,27 +10,27 @@ using Documenter
 
 @info "Building Documentation"
 makedocs(;
-    sitename = "Skeleton",
+    sitename="Skeleton",
     # This argument is only so that the sequence of pages in the sidebar is configured
     # By default all markdown files in `docs/src` are expanded and included.
-    pages = [
-        "index.md",
+    pages=[
+        "index.md"
     ],
     # Don't worry about what `CI` does in this line.
-    format = Documenter.HTML(prettyurls = CI),
+    format=Documenter.HTML(; prettyurls=CI)
 )
 
 @info "Deploying Documentation"
 if CI
-    deploydocs(
+    deploydocs(;
         # `repo` MUST be set correctly. Once your GitHub name is set
         # the auto-generated documentation will be hosted at:
         # https://aksuhton.github.io/Skeleton/dev/
         # (assuming you have enabled `gh-pages` deployment)
-        repo = "github.com/aksuhton/Skeleton.git",
-        target = "build",
-        push_preview = true,
-        devbranch = "main",
+        repo="github.com/aksuhton/Skeleton.git",
+        target="build",
+        push_preview=true,
+        devbranch="main"
     )
 end
 
